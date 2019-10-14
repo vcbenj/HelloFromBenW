8975e6d9a1bf3422293c10acad91a6a512d0e367
vcbenj <whi15026@byui.edu>
2019-10-14 09:22:27 -0600

    Adding

--- src/white/Main.java
+++ src/white/Main.java
@@ -3,6 +3,6 @@ package white;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello my name is Ben!");
+	System.out.println("Hello my name is Ben White!");
     }
 }
