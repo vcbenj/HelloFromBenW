114830d6acfd7f91bbe57327f2091f20ba151122
vcbenj <whi15026@byui.edu>
2019-10-14 10:14:57 -0600

    Adding

--- src/white/Main.java
+++ src/white/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello my name is Ben Whitelkadsflkad");
+        System.out.println("Hello my name is Ben Whitelkaflkad");
         System.out.println("Hello Ben White, my name is Ben too!");
     }
 }
