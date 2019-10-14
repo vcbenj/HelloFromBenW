f438787afd4054b26e6fb9298d244d3c962b8a50
vcbenj <whi15026@byui.edu>
2019-10-14 11:04:36 -0600

    Revert "Adding"
    This reverts commit 8975e6d9a1bf3422293c10acad91a6a512d0e367.
    
    # Conflicts:
    #	src/white/Main.java

--- src/white/Main.java
+++ src/white/Main.java
@@ -3,6 +3,8 @@ package white;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello my name is Ben!");
+        System.out.println("Hello my name is Ben White12");
+        System.out.println("Hello Ben White, my name is Ben too!");
+
     }
 }
